0f07ce16894fc653428bb22d6b3b9e3119f41914
rodrigo <rodrigojfigari@gmail.com>
2023-03-21 20:29:56 -0300

    modificacion del cors url vercel

--- api/src/app.js
+++ api/src/app.js
@@ -20,7 +20,7 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
-    'https://pi-countries-rf.vercel.app/'
+    'https://pi-countries-rf.vercel.app'
   ); // '+'    update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
